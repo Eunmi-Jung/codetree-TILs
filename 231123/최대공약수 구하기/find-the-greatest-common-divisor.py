b5128d0dc65ab11b7f79cd2d099fc0de9c14b748
Eunmi-Jung <150248151+Eunmi-Jung@users.noreply.github.com>
2023-11-23 19:51:39 +0900

    Title: 최대공약수 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231123/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 231123/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -1,11 +1,7 @@
-def GCF(a,b):
-    if a>b:
-        s=a#(다용도 변수)
-    else:
-        s=b
-    for i in range(s-1,0,-1):
+def G(a,b):
+    for i in range(1,min(n,m),+1):
         if a%i==0 and b%i==0:
-            print(i)
-            break
+            g=i
+    print(g)
 n,m=map(int,input().split())
-GCF(n,m)
+G(n,m)
