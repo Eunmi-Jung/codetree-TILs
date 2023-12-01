d9f80d9afd67b623f0e0df502323e0ac39e8c4e0
Eunmi-Jung <150248151+Eunmi-Jung@users.noreply.github.com>
2023-12-01 14:09:13 +0900

    Title: 연속부분수열일까, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231201/연속부분수열일까/contiguous-array-or-not.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include<stdlib.h>
+void input(int *p,int k)
+{
+    int i;
+    for(i=0;i<k;i++)
+     scanf("%d",&p[i]);
+}
+int fun(int *x, int *y, int n, int m)
+{
+    int i,j,start;
+    for(i=0;i<n;i++)
+    {
+        if(x[i]==y[0])
+          {
+             start=i;
+            for(j=1;i<=n;i++)
+              if(j<m && x[start+j]!=y[j]) return 0;
+          
+          }
+    }
+        
+    return 1;
+
+}
+int main() {
+    int n,m;
+    int i,j;
+    int *p1, *p2;
+    int sw;
+    scanf("%d %d",&n,&m);
+    p1=(int *)malloc(sizeof(int)*n);
+    p2=(int *)malloc(sizeof(int)*m);
+    input(p1,n);
+    input(p2,m);
+    sw=fun(p1,p2,n,m);
+    if(sw==1)
+      printf("Yes\n");
+    else
+      printf("No\n");
+    free(p1);
+    free(p2);
+
+    return 0;
+}
